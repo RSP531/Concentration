18573f6a50c6d9a199a3064cd86ed97df099cf35
Rob Peschke <rspesc31@gmail.com>
2020-11-18 12:36:41 -0500

    added concentration game for ios in x code from stanford lecturea

---
+++ ConcentrationStoryboard/ConcentrationStoryboard/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ConcentrationStoryboard
+//
+//  Created by Robert on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    var flipCount = 0 {
+        didSet {
+            FlipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var FlipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🐨","🐼","🐥","🦄"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            print("cardNumber = \(String(cardNumber))")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
